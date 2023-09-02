d8b23b692e89d01b278edbc3bb1d567343db20c6
ariars <ariars@outlook.com>
2023-09-02 23:20:33 +0900

    chapter 03 완료

---
+++ 01.codefactory/ch03/3_2_1_future.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  Future<String> name; // 미래에 받을 String값
+  Future<int> number; // 미래에 받을 int값
+  Future<bool> isOpened; // 미래에 받을 boolean값
+}
