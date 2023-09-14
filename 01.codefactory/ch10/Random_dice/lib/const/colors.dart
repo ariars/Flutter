08469389fd1e9740f167b7f4230eaa6f8f8e961e
ariars <ariars@outlook.com>
2023-09-14 14:35:30 +0900

    chapter 10 완료

---
+++ 01.codefactory/ch10/Random_dice/lib/const/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// 배경색
+const backgroundColor = Color(0xFF0E0E0E);
+
+// 주색상
+const primaryColor = Colors.white;
+
+//보조색상 (600 키값 때문에 const 사용 불가)
+final secondaryColor = Colors.green[600];
