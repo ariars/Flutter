96e3c89d88619a1747cebb838a18d78fb2c857b3
ariars <ariars@outlook.com>
2023-09-16 21:10:22 +0900

    chapter 11 완료

--- 01.codefactory/ch11/vid_player/lib/main.dart
+++ 01.codefactory/ch11/vid_player/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:vid_player/screen/home_screen.dart';
 
-// 기본
-// issue 를 통한 pull request 등록
-// 어휴..
-// pull request
+//
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),
   );
